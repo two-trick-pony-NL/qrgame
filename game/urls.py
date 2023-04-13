3065b26482ffa96999f9c323a769085d00356632
Peter van Doorn <pvandoorn@bunq.com>
2023-04-13 17:16:22 +0200

    update db

--- game/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from qrcodegame import views
-
-urlpatterns = [
-    path('', views.home, name='homepage'),
-    path('random', views.random, name='random'),
-]
