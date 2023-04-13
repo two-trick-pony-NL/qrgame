3c408c9b6f37223a81daf1f6826b6e0a0e802c01
Peter van Doorn <pvandoorn@bunq.com>
2023-04-13 15:51:02 +0200

    sending the url patterns correct

--- core/urls.py
+++ core/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from qrcodegame import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='welcom'),
+    path('', include('qrcodegame.urls')),
+    path('/', include('qrcodegame.urls')), 
     path('healthcheck/', views.home, name='welcom'),
     path('qr/<str:secret>/', views.index, name='home'),
     path('registeruser/<str:secret>/', views.new_user, name='newuser')
